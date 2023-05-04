508ab867c4cb07330a59dec90caf3682ecd18490
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-05-04 08:44:41 +0000

    add questions array

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,3 +3,72 @@ let startQuizButton = document.getElementById("button_start_quiz");
 let restartButton = document.getElementById("button_restart");
 let nextButton = document.getElementById("button_next");
 
+// Questions
+const questionsArray = [
+  {
+      id: "0",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "1",
+      question: "Which is the highest mountain in the Ierland?",
+      options: ["Brandon Peak", "Carrauntoohil", "Caher", "Beenkeragh"],
+      correct: "Carrauntoohil",
+  },
+  {
+      id: "2",
+      question: "Which is the highest mountain in the Germany?",
+      options: ["Watzmann", "Schneefernerkopf", "Zugspitze", "Hochwanner"],
+      correct: "Zugspitze",
+  },
+  {
+      id: "3",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "4",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "5",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+     id: "6",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "7",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "8",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "9",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "10",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+]
