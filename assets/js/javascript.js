f777d372a7c853fc5ce85dfaf394de3774b12c5a
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-05-04 08:23:29 +0000

    add variabels in js file

---
+++ assets/js/javascript.js
@@ -0,0 +1,5 @@
+let quizArea = document.getElementById("questions_area");
+let startQuizButton = document.getElementById("button_start_quiz");
+let restartButton = document.getElementById("button_restart");
+let nextButton = document.getElementById("button_next");
+
