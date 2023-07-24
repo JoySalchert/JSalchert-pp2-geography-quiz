5866d8adc68af53789fb59732b17b15d7938348b
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-07-24 20:42:31 +0000

    add functions to handle quiz cards and to validate the answes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -81,18 +81,6 @@
 // startQuizButton.addEventListener("click", () => {
 //     start
 // });
-
-// // Next Button (User clicks next button)
-// nextButton.addEventListener("click",
-//    (displayNext = () => {
-//     questionCount += 1;
-//     if (questionCount == questionsArray.length) {
-//         quizArea.classList.add("hide");
-//     }
-//     else {
-//         quizArea
-//     }
-//    })
 // )
 
 // // Restart (User clicks restart button)
@@ -101,29 +89,170 @@
 //     displayContainer.classList.remove("hide")
 // })
 
-// // Quiz creation
-// function quizCreator() {
-//     questionsArray.sort (() => Math.random() - 0.5);
-//     for (let i of questionsArray) {
-//         i.options.sort (() => Math.random() - 0.5);
-//         let div = document.createElement("div");
-//         div.classList.add("container-mid", "hide");
-//         countOfQestion.innerHTML = 1 + " of " + questionsArray.length + " Question";
-//         let question_div = document.createElement("p");
-//         question_div.classList.add("question");
-//         question_div.innerHTML = i.question;
-//         div.appendChild(question_div);
-//         div.innerHTML += `
-//         <button class="option-div" onclick="checker (this)">${i.options[0]}</button>
-//         <button class="option-div" onclick="checker (this)">${i.options[2]}</button>
-//         <button class="option-div" onclick="checker (this)">${i.options[3]}</button>
-//         <button class="option-div" onclick="checker (this)">${i.options[4]}</button>
-//         `;
-//         quizArea.appendChild(div);
+//
 //     }
 // }
 
+let questionCount;
+let scoreCount;
 
 function startQuiz() {
-    
+    questionCount = 0;
+    scoreCount = 0;
+    quizCreator();
+    document.getElementById("start_page").classList.add("hide");
+    document.getElementById("question_page").classList.remove("hide");
+    loadQuestion(0);
 }
+
+//Next Button
+function displayNext() {
+    //increment questionCount
+    questionCount += 1;
+    //if last question
+    if (questionCount == questionsArray.length) {
+        //hide question container and display score
+        displayContainer.classList.add("hide");
+        scoreContainer.classList.remove("hide");
+        //user score
+        userScore.innerHTML =
+            "Your score is " + scoreCount + " out of " + questionCount;
+    } else {
+        //display questionCount
+  //      countOfQuestion.innerHTML =
+ //         questionCount + 1 + " of " + questionsArray.length + " Question";
+        //display quiz
+        loadQuestion(questionCount);
+    }
+}
+
+function validateAnswer(userOption) {
+    let userSolution = userOption.value;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+    //if user clicked answer == correct option stored in object
+    if (userSolution === questionsArray[questionCount].correct) {
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+        //For marking the correct option
+        options.forEach((element) => {
+            if (element.value == questionsArray[questionCount].correct) {
+                element.classList.add("correct");
+            }
+        });
+    }
+    //disable all options
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+
+// Questions
+const questionsArray = [
+  {
+      id: "0",
+      question: "Which is the highest mountain in the USA?",
+      options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
+      correct: "Mount Denali",
+  },
+  {
+      id: "1",
+      question: "Which is the highest mountain in the Ierland?",
+      options: ["Brandon Peak", "Carrauntoohil", "Caher", "Beenkeragh"],
+      correct: "Carrauntoohil",
+  },
+  {
+      id: "2",
+      question: "Which is the highest mountain in the Germany?",
+      options: ["Watzmann", "Schneefernerkopf", "Zugspitze", "Hochwanner"],
+      correct: "Zugspitze",
+  },
+  {
+      id: "3",
+      question: "Which is the country with the most time zones (including foreign territories)?",
+      options: ["USA", "France", "Russia", "UK"],
+      correct: "France",
+  },
+  {
+      id: "4",
+      question: "How many continents dose earth have?",
+      options: ["3", "5", "11", "7"],
+      correct: "7",
+  },
+  {
+      id: "5",
+      question: "Which country has the most rain forest?",
+      options: ["Brazil", "Peru", "DRC", "Indonesia"],
+      correct: "Brazil",
+  },
+  {
+     id: "6",
+      question: "In what country is the worlds biggest freshwater lake located?",
+      options: ["Russia", "Canada", "Uganda", "USA"],
+      correct: "Russia",
+  },
+  {
+      id: "7",
+      question: "Which is the smalest country in the world (by land area)?",
+      options: ["Nauru", "Monaco", "San Marino", "Vatican City"],
+      correct: "Vatican City",
+  },
+  {
+      id: "8",
+      question: "Which is the largest country in the world (by land area)?",
+      options: ["China", "Russia", "Canada", "Ausralia"],
+      correct: "Russia",
+  },
+  {
+      id: "9",
+      question: "Which of the following countries is the youngest (date of independance)?",
+      options: ["South Sudan", "Slovakia", "Montenegro", "Palau"],
+      correct: "South Sudan",
+  },
+  {
+      id: "10",
+      question: "How many humans live on earth (in 2023)?",
+      options: ["8 Billion", "10 Billion", "5 Billion", "2 Billion"],
+      correct: "8 Billion",
+  },
+]
+
+// Quiz creation
+function quizCreator() { 
+    let quizArea = document.getElementById("questions_area");
+    questionsArray.sort (() => Math.random() - 0.5);
+    for (let i of questionsArray) {
+        i.options.sort (() => Math.random() - 0.5);
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        let question_div = document.createElement("p");
+        question_div.classList.add("question");
+        question_div.innerHTML = i.question;
+        div.appendChild(question_div);
+        div.innerHTML += `
+        <label>${i.options[0]}</label>
+        <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[0]}"/>
+        <label>${i.options[1]}</label>
+        <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[1]}"/>
+        <label>${i.options[2]}</label>
+        <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[2]}"/>
+        <label>${i.options[3]}</label>
+        <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[3]}"/>
+        `;
+        quizArea.appendChild(div);
+    }
+}
+
+//Display quiz
+function loadQuestion(questionCount) {
+    let quizCards = document.querySelectorAll(".container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+};
