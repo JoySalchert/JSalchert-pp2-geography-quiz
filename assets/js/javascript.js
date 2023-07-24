2f41b8deab6c684e6f1e7445e8274274c6be423c
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-07-24 21:31:16 +0000

    created score_page and retart function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,98 +1,6 @@
 //I took the artikle "Quiz App Using HTML, CSS And JavaScript (Source Code) – Coding Torque"
 //Written by: Piyush Patil (Code Credits: @virtualwiz) as a guide!
 
-// let quizArea = document.getElementById("questions_area");
-// let startQuizButton = document.getElementById("button_start_quiz");
-// let restartButton = document.getElementById("button_restart");
-// let nextButton = document.getElementById("button_next");
-// let questionCount;
-
-// // Questions
-// const questionsArray = [
-//   {
-//       id: "0",
-//       question: "Which is the highest mountain in the USA?",
-//       options: ["Mount Lincoln", "Mount Bona", "Mount Denali", "Mount Saint Elias"],
-//       correct: "Mount Denali",
-//   },
-//   {
-//       id: "1",
-//       question: "Which is the highest mountain in the Ierland?",
-//       options: ["Brandon Peak", "Carrauntoohil", "Caher", "Beenkeragh"],
-//       correct: "Carrauntoohil",
-//   },
-//   {
-//       id: "2",
-//       question: "Which is the highest mountain in the Germany?",
-//       options: ["Watzmann", "Schneefernerkopf", "Zugspitze", "Hochwanner"],
-//       correct: "Zugspitze",
-//   },
-//   {
-//       id: "3",
-//       question: "Which is the country with the most time zones (including foreign territories)?",
-//       options: ["USA", "France", "Russia", "UK"],
-//       correct: "France",
-//   },
-//   {
-//       id: "4",
-//       question: "How many continents dose earth have?",
-//       options: ["3", "5", "11", "7"],
-//       correct: "7",
-//   },
-//   {
-//       id: "5",
-//       question: "Which country has the most rain forest?",
-//       options: ["Brazil", "Peru", "DRC", "Indonesia"],
-//       correct: "Brazil",
-//   },
-//   {
-//      id: "6",
-//       question: "In what country is the worlds biggest freshwater lake located?",
-//       options: ["Russia", "Canada", "Uganda", "USA"],
-//       correct: "Russia",
-//   },
-//   {
-//       id: "7",
-//       question: "Which is the smalest country in the world (by land area)?",
-//       options: ["Nauru", "Monaco", "San Marino", "Vatican City"],
-//       correct: "Vatican City",
-//   },
-//   {
-//       id: "8",
-//       question: "Which is the largest country in the world (by land area)?",
-//       options: ["China", "Russia", "Canada", "Ausralia"],
-//       correct: "Russia",
-//   },
-//   {
-//       id: "9",
-//       question: "Which of the following countries is the youngest (date of independance)?",
-//       options: ["South Sudan", "Slovakia", "Montenegro", "Palau"],
-//       correct: "South Sudan",
-//   },
-//   {
-//       id: "10",
-//       question: "How many humans live on earth (in 2023)?",
-//       options: ["8 Billion", "10 Billion", "5 Billion", "2 Billion"],
-//       correct: "8 Billion",
-//   },
-// ]
-
-// // Quiz Start (User clicks start button)
-// startQuizButton.addEventListener("click", () => {
-//     start
-// });
-// )
-
-// // Restart (User clicks restart button)
-// restartButton.addEventListener("click", () => {
-//     initial ();
-//     displayContainer.classList.remove("hide")
-// })
-
-//
-//     }
-// }
-
 let questionCount;
 let scoreCount;
 
@@ -112,10 +20,10 @@ function displayNext() {
     //if last question
     if (questionCount == questionsArray.length) {
         //hide question container and display score
-        displayContainer.classList.add("hide");
-        scoreContainer.classList.remove("hide");
+        document.getElementById("question_page").classList.add("hide");
+        document.getElementById("score_page").classList.remove("hide");
         //user score
-        userScore.innerHTML =
+        document.getElementById("result").innerHTML =
             "Your score is " + scoreCount + " out of " + questionCount;
     } else {
         //display questionCount
@@ -126,6 +34,7 @@ function displayNext() {
     }
 }
 
+//
 function validateAnswer(userOption) {
     let userSolution = userOption.value;
     let question =
@@ -148,6 +57,12 @@ function validateAnswer(userOption) {
     options.forEach((element) => {
         element.disabled = true;
     });
+}
+
+// Restart (User clicks restart button)
+function restart(){
+    document.getElementById("start_page").classList.remove("hide");
+    document.getElementById("score_page").classList.add("hide");
 }
 
 // Questions
@@ -233,14 +148,17 @@ function quizCreator() {
         question_div.innerHTML = i.question;
         div.appendChild(question_div);
         div.innerHTML += `
-        <label>${i.options[0]}</label>
         <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[0]}"/>
-        <label>${i.options[1]}</label>
+        <label>${i.options[0]}</label>
+        <br>
         <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[1]}"/>
-        <label>${i.options[2]}</label>
+        <label>${i.options[1]}</label>
+        <br>
         <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[2]}"/>
-        <label>${i.options[3]}</label>
+        <label>${i.options[2]}</label>
+        <br>
         <input type="radio" class="option-div" onclick="validateAnswer (this)" value="${i.options[3]}"/>
+        <label>${i.options[3]}</label>
         `;
         quizArea.appendChild(div);
     }
